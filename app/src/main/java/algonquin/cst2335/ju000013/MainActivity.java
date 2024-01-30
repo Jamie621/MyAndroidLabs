b70ebf9a5e01d3ce9e9f07081e2833505861ece7
Jungmin Ju <ju.jungmin@gmail.com>
2024-01-30 17:41:55 -0500

    Week4 : Initial commit

--- app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
@@ -1,14 +1,46 @@
 package algonquin.cst2335.ju000013;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG, "onStart()-The application is niw visible on screen");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG, "onDestroy()-Activity is about to be destroyed, releasing any resources.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG, "onStop()-Activity is no longer visible to the user, it's now stopped.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG, "onPause()-Another activity is taking focus, this activity is about to be paused.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG, "onResume()-The application is now responding to user");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(TAG, "In onCreate()-Activity is being created, setting up the initial state");
     }
 }
