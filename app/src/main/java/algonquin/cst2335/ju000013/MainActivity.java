9d27cff31d972638d4cedaf2929025b4acabd5b4
Jungmin Ju <ju.jungmin@gmail.com>
2024-01-31 00:21:16 -0500

    Week4 : Initial commit

--- app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
@@ -1,46 +1,63 @@
 package algonquin.cst2335.ju000013;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w(TAG, "onStart()-The application is niw visible on screen");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w(TAG, "onDestroy()-Activity is about to be destroyed, releasing any resources.");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w(TAG, "onStop()-Activity is no longer visible to the user, it's now stopped.");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w(TAG, "onPause()-Another activity is taking focus, this activity is about to be paused.");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w(TAG, "onResume()-The application is now responding to user");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w(TAG, "In onCreate()-Activity is being created, setting up the initial state");
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = findViewById(R.id.emailEditText); // Replace with your actual EditText ID
+                String email = emailEditText.getText().toString();
+
+                Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+                nextPage.putExtra( "EmailAddress", emailEditText.getText().toString() );
+                startActivity(nextPage);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG, "onStart() - The application is now visible on screen.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG, "onResume() - The application is now responding to user input.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG, "onPause() - Another activity is taking focus, this activity is about to be paused.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG, "onStop() - The application is no longer visible to the user, it's now stopped.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG, "onDestroy() - Any memory used by the application is freed.");
     }
 }
--- app/src/main/java/algonquin/cst2335/ju000013/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/ju000013/SecondActivity.java
@@ -9,6 +9,21 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the content view to the layout for this activity
         setContentView(R.layout.activity_second);
+
+        // Additional setup to do, you can do it here
+        // For example, setting up listeners for your button
+        // Assume you have a button with ID changePictureButton in your layout
+
+//        Button changePictureButton = findViewById(R.id.changePictureButton);
+//        changePictureButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+                // Handle the button click
+                // For example, launch an intent to pick an image from the gallery
+//            }
+//        });
+
     }
 }
