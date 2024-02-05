f6b7ba5becd470f1182a66af8bc6f1b0306cc8d1
Jungmin Ju <ju.jungmin@gmail.com>
2024-02-05 11:49:57 -0500

    Week4 : Initial commit

--- app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
@@ -17,8 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.w(TAG, "onCreata() - Loading Widgets");
         Button loginButton = findViewById(R.id.loginButton);
+
+//         Use a lambda expression
+
+//        loginButton.setOnClickListener( clk -> {
+//            EditText emailEditText = findViewById(R.id.emailEditText);
+//            String email = emailEditText.getText().toString();
+//
+//            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+//            nextPage.putExtra("EmailAddress", email);
+//            startActivity(nextPage);
+//        });
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,10 +39,19 @@ public class MainActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
 
                 Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-                nextPage.putExtra( "EmailAddress", emailEditText.getText().toString() );
+                nextPage.putExtra( "EmailAddress", emailEditText.getText().toString());
                 startActivity(nextPage);
+
+// In FirstActivity, sending data
+//                Intent next = new Intent(FirstPage.this, NextPage.class);
+//                next.putExtra("Age", 30); // Assuming you want to pass the age as an int
+//                next.putExtra("Name", "Jason"); // Passing a String
+//                next.putExtra("PostalCode", "12345"); // Another String
+//                startActivity(next);
+
             }
         });
+
     }
 
     @Override
--- app/src/main/java/algonquin/cst2335/ju000013/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/ju000013/SecondActivity.java
@@ -1,6 +1,8 @@
 package algonquin.cst2335.ju000013;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,21 +11,26 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set the content view to the layout for this activity
         setContentView(R.layout.activity_second);
 
-        // Additional setup to do, you can do it here
-        // For example, setting up listeners for your button
-        // Assume you have a button with ID changePictureButton in your layout
+        // Retrieve the Intent that started this Activity
+        Intent fromPrevious = getIntent();
 
-//        Button changePictureButton = findViewById(R.id.changePictureButton);
-//        changePictureButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-                // Handle the button click
-                // For example, launch an intent to pick an image from the gallery
-//            }
-//        });
+        // Now retrieve the email address string that was passed with putExtra()
+        if (fromPrevious != null) {
+            String emailAddress = fromPrevious.getStringExtra("EmailAddress");
 
+            // Find the TextView by its ID and set the email address as its text
+            TextView textViewTop = findViewById(R.id.textViewTop);
+            textViewTop.setText(emailAddress); // Set the email address as text
+
+            // In NextPage, retrieving data
+//            Intent fromPrevious = getIntent();
+//            int age = fromPrevious.getIntExtra("Age", -1); // -1 is a default value in case "Age" is not found
+//            String name = fromPrevious.getStringExtra("Name");
+//            String pCode = fromPrevious.getStringExtra("PostalCode");
+
+
+        }
     }
 }
