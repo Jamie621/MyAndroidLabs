726f8bbc1cd09ae84fd28b4ad4bbe6275b4e97f0
Jungmin Ju <ju.jungmin@gmail.com>
2024-02-05 20:11:23 -0500

    Week4 : modified camera permission

--- app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ju000013/MainActivity.java
@@ -1,24 +1,38 @@
 package algonquin.cst2335.ju000013;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     public static final String PREFS_NAME = "MyData"; // Change to public
     public static final String EMAIL_KEY = "LoginName"; // Change to public
     private EditText emailEditText;
+    private static final int CAMERA_PERMISSION_CODE = 123; // You can choose any integer value
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check for camera permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // Request camera permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+
+
+
         Log.w(TAG, "onCreate() - Loading Widgets");
 
         // Initialize the EditText and Button
